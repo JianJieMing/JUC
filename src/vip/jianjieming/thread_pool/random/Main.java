aaabbcb6d3461bab77419dc61cd64f1439ed6767
JianJieMing <1434640043@qq.com>
2019-10-15 10:22:08 +0800

    线程池相关

---
+++ src/vip/jianjieming/thread_pool/random/Main.java
@@ -0,0 +1,29 @@
+package vip.jianjieming.thread_pool.random;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @Classname Main
+ * @Date 2019/7/26 12:03
+ * @Created by jianjieming
+ */
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int num = 0;
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+        Future[] futures = new Future[5];
+        for (int i = 0; i < 5; i++) {
+            Future<?> submit = pool.submit(new RandomCallable());
+            futures[i] = submit;
+        }
+        for (Future future : futures) {
+            Object result = future.get();
+            num += Integer.valueOf(String.valueOf(result));
+        }
+        System.out.println(num);
+        pool.shutdown();
+    }
+}
